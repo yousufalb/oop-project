fad56a2af5acc2f19cbd06ce8987d4746f11a9ce
yousufalb <24-0328@student.gutech.edu.om>
2026-05-12 20:40:34 +0400

    Rental Class
    Made the Rental.h code

--- Rental.h
+++ Rental.h
@@ -5,9 +5,32 @@
 #ifndef UNTITLED2_RENTAL_H
 #define UNTITLED2_RENTAL_H
 
+#include "Car.h"
+#include "Customer.h"
 
 class Rental {
-};
+private:
+    Car* car;
+    Customer* customer;
+    int startDay;
+    int endDay;
+    double totalCost;
+    bool isActive;
 
+public:
+    Rental(Car* car, Customer* customer, int startDay, int endDay);
+
+    Car* getCar();
+    Customer* getCustomer();
+    int getStartDay();
+    int getEndDay();
+    double getTotalCost();
+    bool getIsActive();
+    
+    void setIsActive(bool status);
+    void calculateTotalCost();
+    void displayRental();
+
+};
 
 #endif //UNTITLED2_RENTAL_H

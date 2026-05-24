01a97b869f5f82cf5fefd73bbed43ef919f7a28d
hussainlwt <24-0359@student.gutech.edu.om>
2026-05-24 19:47:50 +0400

    Rental Agency
    Completed Rental Agency H and CPP file

--- RentalAgency.h
+++ RentalAgency.h
@@ -1,13 +1,41 @@
-//
-// Created by yousu on 5/6/2026.
-//
-
 #ifndef UNTITLED2_RENTALAGENCY_H
 #define UNTITLED2_RENTALAGENCY_H
 
+#include <vector>
+#include <string>
+#include "Car.h"
+#include "Customer.h"
+#include "Rental.h"
+#include "Payment.h"
+#include "Sedan.h"
+#include "SUV.h"
+#include "Truck.h"
+#include "Date_Time.h"
+
+
 
 class RentalAgency {
+private:
+    vector<Car*> cars;
+    vector<Customer*> customers;
+    vector<Rental*> rentals;
+    vector<Payment*> payments;
+
+public:
+    RentalAgency();
+    ~RentalAgency();
+
+    void addCar();
+    void addCustomer();
+    void rentCar();
+    void returnCar();
+    void makePayment();
+    void displayCars();
+    void displayCustomers();
+    void displayRentals();
+
 };
+
 
 
 #endif //UNTITLED2_RENTALAGENCY_H
